ff208d80c0039cde83c5eb499e3ba48f6c06e33b
paulooliveira152012 <paulooliveira152012@gmail.com>
2026-03-09 19:46:48 -0300

    adicionando link do vercel no allowFrontend

--- BibliaStudy.Api/Program.cs
+++ BibliaStudy.Api/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3001")
+        policy.WithOrigins("http://localhost:3001", "https://bible-sepia.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
