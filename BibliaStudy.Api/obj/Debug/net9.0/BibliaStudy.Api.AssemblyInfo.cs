92112bf8eb75cc11e3abefaa62261c9548fe4296
paulooliveira152012 <paulooliveira152012@gmail.com>
2026-03-11 20:21:15 -0300

    rodando migration

--- BibliaStudy.Api/obj/Debug/net9.0/BibliaStudy.Api.AssemblyInfo.cs
+++ BibliaStudy.Api/obj/Debug/net9.0/BibliaStudy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70dc14fd78e19150c4d9460e143d9e9afcc2a02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9082fc0ebee1e1cf7045a671d2eb00b8f4dc63dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
