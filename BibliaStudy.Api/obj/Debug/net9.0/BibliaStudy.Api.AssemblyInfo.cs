82a9bc13b0eb6608875072ef7ae2443e5c86f575
paulooliveira152012 <paulooliveira152012@gmail.com>
2026-03-14 09:21:19 -0300

    commit

--- BibliaStudy.Api/obj/Debug/net9.0/BibliaStudy.Api.AssemblyInfo.cs
+++ BibliaStudy.Api/obj/Debug/net9.0/BibliaStudy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc37e424b9a3f0bd4705d5e2802acbb1234748bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e47cc08451296fa3db8d74cc7b216b50ec9508")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliaStudy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
